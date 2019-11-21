c46da5ac4c5c283e634e89722354c9ec4ba7a700
FerhanKushev <ferhan98@gmx.net>
2019-11-21 11:06:47 +0100

    Url Klasse fast fertig gemacht

--- MyWebServer/MyWebServer/Url.cs
+++ MyWebServer/MyWebServer/Url.cs
@@ -7,8 +7,10 @@ namespace MyWebServer
 {
     class Url: IUrl
     {
-        private string rawUrl;
-
+        string rawUrl;
+        string path;
+        Dictionary<string, string> parameterDict;
+        string[] segments;
         public Url(string path)
         {
             this.rawUrl = path;
@@ -22,18 +24,121 @@ namespace MyWebServer
             }
         }
 
-        public string Path => throw new NotImplementedException();
+        public string Path
+        {
+            get
+            {
+                return path;
+            }
+        }
 
-        public IDictionary<string, string> Parameter => throw new NotImplementedException();
+        public IDictionary<string, string> Parameter
+        {
+            get
+            {
+                if (path==null)
+                {
+                    path = "";
+                }
+                string[] tmp = path.Split('?');
+                string[] tmp2 = tmp[1].Split('&');
+                parameterDict.Add("tmp2", tmp2.ToString());
+                return parameterDict;
+            }
+        }
 
-        public int ParameterCount => throw new NotImplementedException();
+        public int ParameterCount
+        {
+            get
+            {
+                if (parameterDict.Count!=0)
+                {
+                    int paraCount = parameterDict.Count;
+                    return paraCount;
+                }
+                return 0;
+            }
+        }
 
-        public string[] Segments => throw new NotImplementedException();
+        public string[] Segments
+        {
+            get
+            {
+                if (path==null)
+                {
+                    path = "";
+                }
+                List<string> resultList = new List<string>();
+                segments = path.Split('/');
+                if (segments.Length <= 0)
+                {
+                    segments[0] = "0";
+                }
+                foreach (var item in segments)
+                {
+                    resultList.Add(item);
+                }
+                return resultList.ToArray();
+            }
+        }
 
-        public string FileName => throw new NotImplementedException();
+        public string FileName
+        {
+            get
+            {
+                if (path==null)
+                {
+                    path = "";
+                }
+                if (segments[segments.Length - 1].Contains(".")==false)
+                {
+                    return "";
+                }
+                string result = segments[segments.Length - 1];
+                string[] tmp = result.Split('#');
+                string fileNameWithExt = tmp[0];
+                return fileNameWithExt;
+            }
+        }
 
-        public string Extension => throw new NotImplementedException();
+        public string Extension
+        {
+            get
+            {
+                if (path == null)
+                {
+                    path = "";
+                }
+                if (segments[segments.Length - 1].Contains(".") == false)
+                {
+                    return "";
+                }
+                string result = segments[segments.Length - 1];
+                string[] tmp = result.Split('#');
+                string fileNameWithExt = tmp[0];
+                string[] tmp2 = fileNameWithExt.Split('.');
+                string ext = tmp2[1];
+                return "." + ext;
+            }
+        }
 
-        public string Fragment => throw new NotImplementedException();
+        public string Fragment
+        {
+            get
+            {
+                if (path == null)
+                {
+                    path = "";
+                }
+                if (segments[segments.Length - 1].Contains(".") == false)
+                {
+                    return "";
+                }
+                string result = segments[segments.Length - 1];
+                string[] tmp = result.Split('#');
+                string fragment = tmp[1];
+                return fragment;
+            }
+        }
     }
 }
